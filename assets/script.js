5c0cfb95b90feb48e296f0ce22ab726a66be3fcf
SevenImm <angelcardenasptx@gmail.com>
2023-09-04 12:49:14 -0500

    "Second iteration of JS added"

--- assets/script.js
+++ assets/script.js
@@ -25,4 +25,4 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic(); 
-//Second iteration
+//Second iteration funtion call + random element
